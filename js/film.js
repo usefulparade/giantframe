d1680d0b0e70ac9f4d8c5a8bda083c38df98264d
usefulparade <breadclip.cooperative@gmail.com>
2019-10-14 19:48:46 -0400

    credits-style site redesign

---
+++ js/film.js
@@ -0,0 +1,22 @@
+// var bgvid;
+// var vidLoaded;
+
+// function preload(){
+//     bgvid = createVideo("./assets/vid.mp4", loadVid);
+// }
+
+// function setup(){
+//     noCanvas();
+//     vidLoaded = false;
+//     bgvid.parent('bgvid');
+// }
+
+// function loadVid(){
+//     bgvid.volume(0);
+//     bgvid.attribute('width', '');
+//     bgvid.attribute('height', '');
+//     vidLoaded = true;
+//     bgvid.play();
+//     bgvid.loop();
+//     bgvid.size(windowWidth, windowHeight);
+// }
